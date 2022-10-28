7149974035b102a2710d034e63d96398bff5b189
POOM <nitipoom_t@cmu.ac.th>
2022-10-29 00:48:24 +0700

    fix people pic

--- src/components/PeopleCard.vue
+++ src/components/PeopleCard.vue
@@ -6,7 +6,7 @@
     >
       <div class="event-card">
         <div class="imgBx">
-          <img :src="people.imageUrls[0]" />
+          <img :src="people.imageUrls" />
         </div>
         <div class="details">
           <span>{{ people.name }} {{ people.surname }}</span>

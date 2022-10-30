72bef8a7f010830ae2795ebedb1ea9bfb5d78a22
Alinjx <chanchanok.2544@gmail.com>
2022-10-31 01:04:03 +0700

    fix inform and vaccine navbar

--- src/views/event/people/PeopleLayoutView.vue
+++ src/views/event/people/PeopleLayoutView.vue
@@ -1,14 +1,24 @@
 <template>
   <div v-if="GStore.people">
     <div id="nav">
-      <router-link :to="{ name: 'PeopleInformations' }"
-        >Information</router-link
-      >
-      |
-      <router-link :to="{ name: 'PeopleVaccine' }">Vaccines</router-link>
-      |<span v-if="isDoctor">
-        <router-link :to="{ name: 'AddComment' }">Add Comment</router-link>
-      </span>
+      <nav class="navi">
+        <a>
+              <router-link :to="{ name: 'PeopleInformations' }"
+          >Information</router-link
+        >
+        </a>
+    |
+<a>
+     <router-link :to="{ name: 'PeopleVaccine' }">Vaccines</router-link>
+</a>
+|
+     <a>
+       <span v-if="isDoctor">
+          <router-link :to="{ name: 'AddComment' }">Add Comment</router-link>
+        </span>
+     </a>
+     <div class="dot"></div>
+      </nav>
     </div>
     <router-view :people="GStore.people" />
   </div>
@@ -33,3 +43,15 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+#nav {
+  background-color: #212121;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+#nav a :hover{
+  color: #fddb3a;
+}
+</style>

f09e5d6d0d1676e514459a61584297d5beec29b6
POOM <nitipoom_t@cmu.ac.th>
2022-10-31 17:22:21 +0700

    deploy(3)

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: 'http://3.85.67.171:8999',
 
   headers: {
     Accept: 'application/json',
